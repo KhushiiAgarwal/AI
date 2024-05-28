a1ddefe466f3ee10400f09389369e7fbb41683cf
Khushi A <99123453+KhushiiAgarwal@users.noreply.github.com>
2024-05-28 21:19:55 +0530

    Analysis

---
+++ CSP & Prolog/ESystem.pl
@@ -0,0 +1,16 @@
+% Rule
+sentiment(X):-positive(X),negative(X),neutral(X).
+positive(good).
+positive(happy).
+positive(lovely).
+positive(nice).
+positive(amazing).
+negative(bad).
+negative(sad).
+negative(dissatisfied).
+negative(wrong).
+negative(worst).
+neutral(ok).
+neutral(fine).
+neutral(middlegrond).
+
